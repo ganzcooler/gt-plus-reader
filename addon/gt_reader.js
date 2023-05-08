19a5563ddc03f7f73c4e1096647d689a57577b87
Daniel Kreitz-Evers <daniel.kreitz-evers@sartorius.com>
2023-05-08 14:18:51 +0200

    Add support for listt

--- addon/gt_reader.js
+++ addon/gt_reader.js
@@ -8,7 +8,7 @@ for (let i = 0; i < all_header.length; i++) {
   }
 }
 
-var sourcecode = document.getElementsByTagName("html")[0].innerHTML;
+//var sourcecode = document.getElementsByTagName("html")[0].innerHTML; // not needed? DELETE
 var article = header.parentElement;
 
 // Check if article is PLUS article
@@ -24,7 +24,6 @@ if (article.lastChild.lastChild.nodeName == "svg") {
   var script = document.getElementById("fusion-metadata").innerHTML;
   eval(script); // window variable zu diesem zeitpunkt das objekt "Fusion" hinzufügen um darauf zugreifen zu können
   var global_content = window.Fusion.globalContent.elements;
-  console.log(global_content);
   
   // Get p tags class name
   var p_tag_class_name = header.lastChild.lastChild.getElementsByTagName("p")[0].className;
@@ -38,27 +37,33 @@ if (article.lastChild.lastChild.nodeName == "svg") {
       full_article += "<h2 class=\"" + h2_tag_class_name + "\">";
       full_article += global_content[i]["text"];
       full_article += "</h2>";
-      continue;
     }
 
-    if (global_content[i]["type"] == "text") {
+    else if (global_content[i]["type"] == "text") {
       full_article += "<p class=\"" + p_tag_class_name + "\">";
       full_article += global_content[i]["text"];
       full_article += "</p>";
-      continue;
     }
 
-    if (global_content[i]["type"] == "image") {
+    else if (global_content[i]["type"] == "image") {
       full_article += "<img src=\"";
       full_article += global_content[i]["imageInfo"]["src"] + "\"" + " ";
       full_article += "style=\"" + "max-width: 100\%;height: auto;\"" + "/>";
       full_article += "<figcaption class=\"" + p_tag_class_name + "\"" + ">";
       full_article += global_content[i]["imageInfo"]["caption"] + "</figcaption>";
-      continue;
+    }
+
+    else if (global_content[i]["type"] == "list") {
+      full_article += "<p class=\"" + p_tag_class_name + "\">";
+      
+      for (let j = 0; j < global_content[i]["list"]["items"].length; j++) {
+        full_article += global_content[i]["list"]["items"][j]["text"] + "<br>";
+      }
+
+      full_article += "</p>";
     }
   }
 
-  // Replace text with full article
   // Get all p tags in div and delete them
   var p_tags_article = header.lastChild.lastChild.getElementsByTagName("p");
 
@@ -66,7 +71,7 @@ if (article.lastChild.lastChild.nodeName == "svg") {
     p_tags_article[i].remove();
   }
 
-  // Actual replacement
+  // Replace text with full article
   header.lastChild.lastChild.innerHTML = full_article;
 
   // Remove bottom gradient
